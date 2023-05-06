2cc192cc4b30f2a04a5ce109d9719508c54fde09
XSlimeNB <116885083+W9pi3cZ1@users.noreply.github.com>
2023-05-07 07:10:49 +0800

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,39 +1,3 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWHreyHcjLQlHcDyxMGK3A5s+1Mz+/OGTrV0hee0sa6nJyvqML/Qv+lfrf5MuwkZYjcIACCdIbBWmUgI1oGAMtGWRVw==
-::fBE1pAF6MU+EWHreyHcjLQlHcDyxMGK3A5s+1Mz+/OGTrV0hee0sa6nJyvqML/Qv+lfrf5MuwkZYjcIACCdIbBWmUh0hpyBIpQQ=
-::fBE1pAF6MU+EWHreyHcjLQlHcDyxMGK3A5s+1Mz+/OGTrV0hee0sa6nJyvqML/Qv+lfrf5MuwkZYjcIACCdLdxWlZwExrQ4=
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFCNnQA2POUWYOpEZ++Pv4Pq7j0IOR8QtaMHZ1aCyMPIc4EDnYaovw3NWnPddQhJbcXI=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSTk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZkoFGErXXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlXMaAs=
-::ZQ05rAF9IAHYFVzEqQIdJwhHahaSHX6uCYYV7+bS4Yo=
-::eg0/rx1wNQPfEVWB+kM9LVsJDCmNL3icFKU9/fz03OOArGQTNA==
-::fBEirQZwNQPfEVWB+kM9LVsJDFHXbE3a
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWNxhPMTg2Z68moBfwO9g3DHHJIg=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFCNnQA2POUWYOpEZ++Pv4Pq7j0IOR8QtaMHZ1aCyMPIc4EDnYaovw3NWnPcfDRVcfB2zUh8mrmN+lHGMO8PSthfkKg==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 chcp 65001
 setlocal enabledelayedexpansion
